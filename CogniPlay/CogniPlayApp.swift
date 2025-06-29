848be4855d541fbaac349f94bda6496f3017fb0a
Ch1mkinCommit <114319585+Ch1mkinCommit@users.noreply.github.com>
2025-06-28 21:00:43 -0700

    Initial Commit

---
+++ CogniPlay/CogniPlayApp.swift
@@ -0,0 +1,17 @@
+//
+//  CogniPlayApp.swift
+//  CogniPlay
+//
+//  Created by Lucas Lum on 6/28/25.
+//
+
+import SwiftUI
+
+@main
+struct CogniPlayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CogniPlay/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CogniPlay
+//
+//  Created by Lucas Lum on 6/28/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
